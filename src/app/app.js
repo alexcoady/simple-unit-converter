d25eb011277c0a9a9e3d8147ecfa2c07ef082c14
Alex Coady <alexandercoady@me.com>
2014-02-09 22:50:19 +0000

    Started styling app - in LESS currently, will need converting to Sass. Have mostly used plain CSS

--- src/app/app.js
+++ src/app/app.js
@@ -17,8 +17,5 @@ angular.module( 'ngBoilerplate', [
 
     $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
         
-        if ( angular.isDefined( toState.data.pageTitle ) ) {
-            $scope.pageTitle = toState.data.pageTitle + ' | ngBoilerplate';
-        }
     });
 });
