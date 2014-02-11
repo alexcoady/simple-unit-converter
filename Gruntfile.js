5606c92c9d57e5d431bc99c00a0d54c632e843e6
Mike Snow <m@alleviate>
2014-02-11 22:32:57 +0000

    StLots of styling and transition changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -371,7 +371,7 @@ module.exports = function ( grunt ) {
         * plugin should auto-detect.
         */
         options: {
-            livereload: true
+            livereload: false
         },
 
         /**
